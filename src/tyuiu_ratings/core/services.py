20fe9fa5196ad5f1850fb8f684fb61399b597c4c
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-20 01:02:41 +0500

    20.05.2025  add critical notifier

--- src/tyuiu_ratings/core/services.py
+++ src/tyuiu_ratings/core/services.py
@@ -3,10 +3,17 @@ from typing import Optional
 from .dto import ApplicantReadDTO
 from .domain import Notification, History
 from .templates import POSITIVE_MESSAGE, WARNING_MESSAGE, CRITICAL_MESSAGE
-from .interfaces import BaseNotifier, ProfileRepository, HistoryRepository
+from .interfaces import (
+    BaseNotifier,
+    ProfileRepository,
+    HistoryRepository,
+    RecommendationSystem
+)
 from ..constants import (
-    THRESHOLD_PROBABILITY,
-    THRESHOLD_VELOCITY
+    POSITIVE_THRESHOLD_PROBABILITY,
+    CRITICAL_THRESHOLD_PROBABILITY,
+    THRESHOLD_VELOCITY,
+    BUDGET_PLACES_FOR_DIRECTIONS
 )
 
 
@@ -29,7 +36,7 @@ class PositiveNotifier(BaseNotifier):
             )
 
     async def _check_conditional(self, applicant: ApplicantReadDTO) -> bool:
-        return applicant.probability >= THRESHOLD_PROBABILITY
+        return applicant.probability >= POSITIVE_THRESHOLD_PROBABILITY
 
 
 class WarningNotifier(BaseNotifier):
@@ -62,3 +69,31 @@ class WarningNotifier(BaseNotifier):
     async def _check_conditional(self, history: History) -> bool:
         last_velocity = history.velocity[-1]
         return last_velocity <= THRESHOLD_VELOCITY
+
+
+class CriticalNotifier(BaseNotifier):
+    def __init__(
+            self,
+            profile_repository: ProfileRepository,
+            recommendation_system: RecommendationSystem
+    ) -> None:
+        self.profile_repository = profile_repository
+        self.recommendation_system = recommendation_system
+
+    async def create_notification(self, applicant: ApplicantReadDTO) -> Optional[Notification]:
+        if not await self._check_conditional(applicant):
+            return None
+        profile = await self.profile_repository.get_by_applicant_id(applicant.applicant_id)
+        if profile:
+            return Notification(
+                level="CRITICAL",
+                user_id=profile.user_id,
+                text=CRITICAL_MESSAGE.format(
+                    direction=applicant.direction,
+                    probability=applicant.probability
+                )
+            )
+
+    async def _check_conditional(self, applicant: ApplicantReadDTO) -> bool:
+        return applicant.probability <= CRITICAL_THRESHOLD_PROBABILITY \
+            or applicant.rating < BUDGET_PLACES_FOR_DIRECTIONS[applicant.direction]
