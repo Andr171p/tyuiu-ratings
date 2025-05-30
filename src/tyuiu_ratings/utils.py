dab9af645577a14cacc6a869d3b16814da21a51c
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-30 13:00:47 +0500

    30.05.2025 Add docstring to utils

--- src/tyuiu_ratings/utils.py
+++ src/tyuiu_ratings/utils.py
@@ -9,11 +9,13 @@ from .constants import DIRECTIONS_MAPPING_CSV
 
 
 def calculate_pages(total_count: int, limit: int) -> int:
+    """Рассчитывает количество страниц для пагинации"""
     pages = total_count // limit
     return pages
 
 
 def calculate_velocity(history: list["RatingPosition"]) -> list[float]:
+    """Вычисляет изменения позиций в рейтинге"""
     df = pd.DataFrame([rank.model_dump() for rank in history])
     df.set_index("date", inplace=True)
     df["rating_change"] = df["rating"].diff().fillna(0)
@@ -21,6 +23,7 @@ def calculate_velocity(history: list["RatingPosition"]) -> list[float]:
 
 
 def calculate_mean_velocity(history: list["RatingPosition"]) -> float:
+    """Вычисляет среднее изменение в позиции в рейтинге"""
     df = pd.DataFrame([rank.model_dump() for rank in history])
     df.set_index("date", inplace=True)
     df["rating_change"] = df["rating"].diff().fillna(0)
@@ -28,6 +31,7 @@ def calculate_mean_velocity(history: list["RatingPosition"]) -> float:
 
 
 def calculate_acceleration(history: list["RatingPosition"]) -> list[float]:
+    """Вычисляет скорость изменений позиций в рейтинге"""
     df = pd.DataFrame([rank.model_dump() for rank in history])
     df.set_index("date", inplace=True)
     df["rating_change"] = df["rating"].diff().fillna(0)
@@ -36,12 +40,14 @@ def calculate_acceleration(history: list["RatingPosition"]) -> list[float]:
 
 
 def calculate_stability(history: list["RatingPosition"]) -> float:
+    """Вычисляет стабильность изменений в рейтинге"""
     df = pd.DataFrame([rank.model_dump() for rank in history])
     df.set_index("date", inplace=True)
     return df["rating"].std()
 
 
 def is_rating_stable(history: list["RatingPosition"], days_count: int, max_change: int) -> bool:
+    """Проверяет стабильность позиции в рейтинге за N-ое количество дней """
     df = pd.DataFrame([rank.model_dump() for rank in history])
     df.set_index("date", inplace=True)
     cutoff_date = df.index.max() - pd.Timedelta(days=days_count)
